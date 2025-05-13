ec64cad78e8b006fcfa36af52543b81d31ef710f
wowyesol <bmo11152598@gmail.com>
2025-05-13 23:51:44 +0900

    0513_start

---
+++ Assets/Script/SceneChange.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChange : MonoBehaviour
+{
+    private UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor socket;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
